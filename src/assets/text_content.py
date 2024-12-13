e69d8b4350f432ce1f3e783b3b33e3a94734d06e
kushal-10 <kushalkoshti11@gmail.com>
2024-12-13 11:41:08 +0100

    update repo name

--- src/assets/text_content.py
+++ src/assets/text_content.py
@@ -1,6 +1,6 @@
 TITLE = """<h1 align="center" id="space-title"> 🏆 CLEM Leaderboard</h1>"""
 
-REPO = "https://raw.githubusercontent.com/kushal-10/clembench-runs/main/"
+REPO = "https://raw.githubusercontent.com/clembench/clembench-runs/main/"
 REGISTRY_URL = "https://raw.githubusercontent.com/clp-research/clembench/refs/heads/main/backends/model_registry.json"
 BENCHMARK_FILE = "benchmark_runs.json"
 
